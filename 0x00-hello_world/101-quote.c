5c97b26a06eedbd29ddc94a6ba5aaefa75dcb83d
Henriques Adriano Monteiro <henriqueadriano38@gmail.com>
2023-08-13 12:05:53 -0700

    Advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,11 @@
 #include<unistd.h>
 
+/**
+ * main - Entry point
+ * Description: Prints a string using write function
+ * Return: 1 - Always fail
+ */
+
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
