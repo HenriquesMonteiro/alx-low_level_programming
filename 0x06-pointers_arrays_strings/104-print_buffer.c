3c93b89129223418ece1f4f677c642abd4a09454
Henriques Adriano Monteiro <henriqueadriano38@gmail.com>
2023-09-07 04:40:18 -0700

    project 6

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,45 +1,52 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * infinite_add - adds two numbers
- * @n1: first number
- * @n2: second number
- * @r: buffer for result
- * @size_r: buffer size
+ * print_buffer - prints a buffer of size bytes by 'b'
+ * @b: buffer to be printed
+ * @size: size of buffer to print
  *
- * Return: address of r or 0
+ * Return: void
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+void print_buffer(char *b, int size)
 {
-int i, j, k, l, m, n;
-for (i = 0; n1[i]; i++)
-;
-for (j = 0; n2[j]; j++)
-;
-if (i > size_r || j > size_r)
-return (0);
-m = 0;
-for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
-{
-n = m;
-if (i >= 0)
-n += n1[i] - '0';
-if (j >= 0)
-n += n2[j] - '0';
-if (i < 0 && j < 0 && n == 0)
-{
-break;
-}
-m = n / 10;
-r[k] = n % 10 + '0';
-}
-r[k] = '\0';
-if (i >= 0 || j >= 0 || m)
-return (0);
-for (k -= 1, l = 0; l < k; k--, l++)
-{
-m = r[k];
-r[k] = r[l];
-r[l] = m;
-}
-return (r);
+	int i, j, bytes = 0;
+
+	if (size <= 0)
+		printf("\n");
+	else
+	{
+		for (i = 0; i < size; i += 10)
+		{
+			printf("%08x: ", i);
+
+			for (j = 0; j < 10; j++)
+			{
+				if (j == 0)
+					bytes = i;
+				if (bytes >= size)
+				{
+					printf("  ");
+				}
+				else
+				{
+					printf("%02x", b[bytes]);
+					bytes++;
+				}
+				if (j % 2)
+					printf(" ");
+			}
+			for (j = 0; j < 10; j++, bytes++)
+			{
+				if (j == 0)
+					bytes = i;
+				if (bytes >= size)
+					break;
+				if ((b[bytes] < 32 || b[bytes] > 126))
+					printf(".");
+				else
+					printf("%c", b[bytes]);
+			}
+			printf("\n");
+		}
+	}
 }
